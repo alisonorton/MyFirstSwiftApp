a210532a6663c2b045ede6d16cfe4d16ad962d13
alisonorton <orton.alison@gmail.com>
2022-10-10 13:39:57 -1000

    Initial Commit

---
+++ SimpleApp/SimpleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleAppApp.swift
+//  SimpleApp
+//
+//  Created by Alison Orton on 10/10/22.
+//
+
+import SwiftUI
+
+@main
+struct SimpleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
